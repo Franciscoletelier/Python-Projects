3403ca5e095a4fa22114a5dd9d3d4b8e560464f6
Francisco Letelier <48863857+Franciscoletelier@users.noreply.github.com>
2022-10-03 22:47:09 -0300

    Delete ejercicio1.py

--- ejercicio1.py
+++
@@ -1,9 +0,0 @@
-#No colocar acentos en el comentario por que no los pesca
-edad = int(input("Por favor, escribe tu edad: "))
-if edad < 18:
-    print("Eres menor de edad")
-    nombre = str(input("Escribe tu nombre")))
-if edad > 30:
-    print ("Eres adulto")
-else:
-    print("Eres mayor de edad")
